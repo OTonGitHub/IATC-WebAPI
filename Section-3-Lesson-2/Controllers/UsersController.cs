0edc6e18530efbd793683e3a730a5cfdbc584777
Ifhaam-CSC <ahmed.ifhaam@csc.gov.mv>
2023-04-07 09:59:47 +0500

    Switched UsersController to file-scoped namespace

--- Section-3-Lesson-2/Controllers/UsersController.cs
+++ Section-3-Lesson-2/Controllers/UsersController.cs
@@ -1,11 +1,11 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Section_3_Lesson_2.Controllers
+// switched to file-scoped name space, instead of encapsulating all in brace
+namespace Section_3_Lesson_2.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UsersController : ControllerBase
-    {
-    }
 }
