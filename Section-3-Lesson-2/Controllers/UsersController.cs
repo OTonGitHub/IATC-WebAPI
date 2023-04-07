9dfe0afc9a44857142f69f2a26c54336da097fb1
Ifhaam-CSC <ahmed.ifhaam@csc.gov.mv>
2023-04-07 09:57:29 +0500

    Added UsersController

---
+++ Section-3-Lesson-2/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Section_3_Lesson_2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
