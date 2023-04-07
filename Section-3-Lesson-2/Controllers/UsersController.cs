1e6278ab7cfc981dcdf93d84d172c897407f3bb2
OTonGitHub <ahmed.ifhaam.work@outlook.com>
2023-04-07 15:36:27 +0500

    Added API Verbs with Function (out-of-band)
    - added some functions to API watching the video on my own. Watching a bit of kudvenkat and IAmTimCorey, I wrote the functions myself to test it out, ahead of the video.

--- Section-3-Lesson-2/Controllers/UsersController.cs
+++ Section-3-Lesson-2/Controllers/UsersController.cs
@@ -1,11 +1,77 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 // switched to file-scoped name space, instead of encapsulating all in brace
 namespace Section_3_Lesson_2.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UsersController : ControllerBase
+public class UsersController : ControllerBase // file was plural, but class name set singular, why?
 {
+    // more on readonly collections: https://www.youtube.com/watch?v=7hBPI0xYezo
+    private readonly static List<string> users = new() // no paranthesis ? if List<string>()
+    {
+        "Ali",
+        "Aisha",
+        "Alex",
+        "Sue"
+    };
+
+    // GET: api/users<controller>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return users;
+    }
+
+    // GET: api/users<controller>/1
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        try
+        {
+            return users[id];
+        }
+        catch (Exception ex)
+        {
+            return ex.Message;
+        }
+
+    }
+
+    // POST: api/users<controller>/value
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+        users.Add(value);
+    }
+
+    // DELETE: api/users<controller>/1
+    [HttpDelete("{id}")]
+    public HttpStatusCode Delete(int id)
+    {
+        try
+        {
+            users.RemoveAt(id);
+            return HttpStatusCode.OK;
+        }
+        catch {
+            return HttpStatusCode.InternalServerError;
+        }
+    }
+
+    // PUT: api/users<controller>/1
+    [HttpPut("{id}")]
+    public HttpStatusCode Put(int id, [FromBody] string value)
+    {
+        try
+        {
+            users[id] = value;
+            return HttpStatusCode.OK;
+        }
+        catch {
+            return HttpStatusCode.InternalServerError;
+        }
+    }
 }
