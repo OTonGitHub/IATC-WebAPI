e5d904b1ac91dc5d261f7a64ad74e6f56416c185
Ahmed.Ifhaam <ahmed.ifhaam.work@outlook.com>
2023-04-15 03:19:50 +0500

    Added swashbuckle, API endpoints & Swagger UI
    - Having /api/* hardcoded in the URL helps identifying if something came from app or api side, for example errors.
    - To an existing project, just added controller service, and mapped controllers.
    - For swagger, swashbuckle from nuget, and then api explorer service, swaggergen service and using swagger and swaggerUI.

--- Section-3-Lesson-8/Program.cs
+++ Section-3-Lesson-8/Program.cs
@@ -3,6 +3,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllers(); // controller with view, is MVC
+builder.Services.AddEndpointsApiExplorer(); // not swagger dependent
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +14,11 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
+else // only in development
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
